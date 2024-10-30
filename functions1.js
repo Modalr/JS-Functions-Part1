aba8c9b6918bf030c447a23901d485bc6d0e9996
Moudhi AlRubaiaan <malrubaian@hotmail.com>
2024-10-30 19:05:19 +0300

    Functiontask

---
+++ functions1.js
@@ -0,0 +1,29 @@
+function printName() {
+  console.log("Moudhi");
+}
+
+function printAge(birthYear) {
+  console.log("2024 - birthYear");
+}
+
+function PrintNameAndAge(birthYear, name) {
+  console.log("Hello ${name{name} you are ${2024 - birthYear} years old");
+}
+
+function printHello(name, language) {
+  if (language === "en") {
+    console.log("Hello {$ name}");
+  } else if (language === "es") {
+    console.log("Hola {$ name}");
+  } else if (language === "fr") {
+    console.log("Bonjour {$ name}");
+  } else if (language === "tr") {
+    console.log("Merhaba ${name}");
+  }
+}
+
+function printMax(x, y) {
+  if (x > y) {
+    console.log(x);
+  } else console.log(y);
+}
